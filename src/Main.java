270217219f183b2da095e5eb8800dc17f6b56907
aimadkha <aimadkha@gmail.com>
2024-11-28 23:04:38 +0100

    add tasks added

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,50 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import model.Task;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class Main {
     public static void main(String[] args) {
+        List<Task> tasks = new ArrayList<>();
+        choice(tasks, args);
+
+        for (Task task : tasks) {
+            System.out.println("tasks are : "+task.getDescription());
+        }
+
+
+
+    }
+
+    private static void choice(List<Task> tasks,String[] args) {
+        for (String arg : args) {
+            System.out.println(arg);
+        }
+//        while (args.length > 0 && !args[0].equals("quit")) {
+        switch (args[0]){
+            case "add":
+                addTasks(tasks,args[1]);
+                break;
+            case "remove":
+                removeTask(tasks,args[1]);
+                break;
+        }
+//        }
+    }
+
+
+    private static void addTasks(List<Task> tasks, String task) {
+        Task newTask = new Task();
+        newTask.setCreatedAt(LocalDate.now());
+        newTask.setDescription(task);
+        tasks.add(newTask);
+    }
+
+    private static void removeTask(List<Task> tasks,String task) {}
+
+    private void write(Task task) {
 
     }
 }
---
+++ src/model/Task.java
@@ -0,0 +1,78 @@
+package model;
+
+import java.time.LocalDate;
+
+public class Task {
+
+    private Long id;
+
+    private String title;
+
+    private String description;
+
+    private Status status;
+
+    private LocalDate createdAt;
+
+    private LocalDate updatedAt;
+
+    public Task() {
+    }
+
+    public Task(Long id, String title, String description, Status status, LocalDate createdAt, LocalDate updatedAt) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.status = status;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    public LocalDate getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDate createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDate getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDate updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+}
